1dda531f92c2b158b98328bf25747821d6996d88
softhea <liviusandulache@yahoo.com>
2023-12-01 11:25:51 +0200

    Update User.php

--- 01/app/Models/User.php
+++ 01/app/Models/User.php
@@ -19,8 +19,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        // 'username',
-        // 'role_id',
         'email',
         'password',
     ];
